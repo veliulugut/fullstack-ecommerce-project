38f969c216227de093b31bd66f9dcab5c91a0f83
Veli Ulugut <veliulugut1@gmail.com>
2024-08-24 19:25:00 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI,Request
+import uvicorn
+
+app = FastAPI()
+
+
+@app.get("/")
+def main(request:Request)-> dict:
+    return{
+        "Message":"Hello World",
+    }
+    
+
+if __name__ == "__main__":
+    uvicorn.run("main:app",host="0.0.0.0",port=8080)
