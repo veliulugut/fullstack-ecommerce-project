9a2bea3c22a475670297f25191c72a44ace9ec27
Veli Ulugut <veliulugut1@gmail.com>
2024-08-26 16:10:08 +0300

    update

--- main.py
+++ main.py
@@ -12,4 +12,4 @@ def main(request:Request)-> dict:
     
 
 if __name__ == "__main__":
-    uvicorn.run("main:app",host="0.0.0.0",port=8080)
+    uvicorn.run("main:app",host="0.0.0.0",port=8080,reload=True)
