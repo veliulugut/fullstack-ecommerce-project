4ec134886a945abacae832b3f1283c649ce85271
Veli Ulugut <veliulugut1@gmail.com>
2024-08-29 16:59:22 +0300

    added token

--- api/user.py
+++ api/user.py
@@ -1,13 +1,17 @@
-from fastapi import APIRouter,Depends,Form,HTTPException
+from fastapi import APIRouter,Depends,Form,HTTPException,Response
 from sqlalchemy.orm import Session
 from database.connection import sess_db
-from security.secur import get_password_hash
+from security.secur import get_password_hash,verify_password,create_access_token,verify_token
+from starlette.responses import RedirectResponse
 
 #Repository
 from repository.user import UserRepository
 
 #Model
 from models.user import UserModel
+
+#security
+from security.secur import COOKIE_NAME
 
 router = APIRouter()
 
@@ -28,3 +32,42 @@ def signup_user(db:Session=Depends(sess_db),username : str = Form(),email:str=Fo
             status_code=401,detail = "Credentials not correct"
         )
     
+
+
+@router.post("/signin")
+def sigin_user(response:Response,db:Session=Depends(sess_db),username : str = Form(),password:str=Form()):
+    userRepository = UserRepository(db)
+    db_user = userRepository.get_user_by_username(username)
+    if not db_user:
+        return "username or password is not valid"
+    
+    if verify_password(password,db_user.password):
+        token = create_access_token(db_user)
+        response.set_cookie(
+            key=COOKIE_NAME,
+            value=token,
+            httponly=True,
+            expires=1800
+        )
+        return {COOKIE_NAME:token,"token_type":"bearer"}
+    
+
+@router.get('/verify/{token}')
+def verify_user(token,db:Session=Depends(sess_db)):
+    userRepository = UserRepository(db)
+    payload = verify_token(token)
+    username = payload.get("sub")
+    db_user = userRepository.get_user_by_username(username)
+
+    if not username:
+        raise HTTPException(
+            status_code=401,detail="Invalid token"
+        )
+    
+    if db_user.is_active == True:
+        return " your account has been allready activated"
+    db_user.is_active=True
+    db.commit()
+    response = RedirectResponse(url="/user/signin")
+    return response
+
--- security/secur.py
+++ security/secur.py
@@ -7,6 +7,8 @@ import logging
 import sys
 from datetime import datetime, timedelta
 import jwt 
+from fastapi import Depends,Request
+from models.user import UserModel
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
@@ -16,6 +18,7 @@ JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM", "HS256")  
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))  
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+#Save to token
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="user/signin")
 COOKIE_NAME = "Authorization"
 
@@ -30,29 +33,41 @@ def create_access_token(user):
             "exp": expire,
         }
         token = jwt.encode(payload, JWT_SECRET_KEY, algorithm=ALGORITHM)
-        logging.info(f"Token oluşturuldu: {token}")
+        logging.info(f"Token created: {token}")
         return token
     except Exception as e:
-        logging.error(f"Token oluşturma hatası: {e}")
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Token oluşturma başarısız")
+        logging.error(f"Token creation failed: {e}")
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Token creation failed")
 
 def verify_token(token):
     try:
         payload = jwt.decode(token, JWT_SECRET_KEY, algorithms=[ALGORITHM])
-        logging.info(f"Token içeriği: {payload}")
+        logging.info(f"Token content: {payload}")
         return payload
     except jwt.ExpiredSignatureError:
-        logging.error("Token süresi dolmuş")
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token süresi dolmuş")
+        logging.error("Token expired")
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token expired")
     except jwt.InvalidTokenError:
-        logging.error("Geçersiz token")
+        logging.error("Invalid token")
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Geçersiz token")
     except Exception as e:
-        logging.error(f"Token doğrulama hatası: {e}")
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Token doğrulama başarısız")
+        logging.error(f"Token validation failure: {e}")
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Token validation failure")
 
 def get_password_hash(password: str) -> str:
     return pwd_context.hash(password)
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_current_user_from_token(token:str=Depends(oauth2_scheme)):
+    user=verify_token(token)
+    return user
+
+def get_current_user_from_cookie(request:Request)-> UserModel:
+    token=request.cookies.get(COOKIE_NAME)
+    if token:
+        user = verify_token(token)
+        return user
+    
