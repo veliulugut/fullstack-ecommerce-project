2eb14e87f198f4e1fe83135e7209dca2d5c6f433
Veli Ulugut <veliulugut1@gmail.com>
2024-08-29 21:57:15 +0300

    comment lines deleted

--- database/connection.py
+++ database/connection.py
@@ -7,20 +7,17 @@ import psycopg2 as db
 
 load_dotenv()
 
-# Çevre değişkenlerini alın
 POSTGRES_USER = os.getenv("POSTGRES_USER")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 POSTGRES_DB = os.getenv("POSTGRES_DB")
 
-# PostgreSQL bağlantı dizesini oluşturun
 database_connection = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{DB_HOST}:{DB_PORT}/{POSTGRES_DB}"
 
 print(f"Database connection URL: {database_connection}")
 
 try:
-    # Bağlantıyı kontrol et
     conn = db.connect(
         host=DB_HOST,
         dbname=POSTGRES_DB,
@@ -33,12 +30,10 @@ try:
 except db.OperationalError as e:
     print(f"Error connecting to database: {e}")
 
-# SQLAlchemy yapılandırmasını oluşturun
 engine = create_engine(database_connection)
 SessionFactory = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-# Veritabanı tablolarını oluştur
 Base.metadata.create_all(bind=engine)
 
 def sess_db():
